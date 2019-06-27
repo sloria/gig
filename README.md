59f6fe4c9412d313a60be82c9ee89af5a274c0c8
Steven Loria <sloria1@gmail.com>
2019-06-27 19:50:42 -0400

    Resurrection! (#3)
    * Modernize the codebase
    * Write to stdout instead of clobbering files
    * Only support Python >= 3.6

---
+++ README.md
@@ -0,0 +1,75 @@
+# gig
+
+[![PyPI Latest Version](https://badgen.net/pypi/v/gig)](https://pypi.org/project/gig/)
+[![Travis CI Status](https://badgen.net/travis/sloria/gig)](https://travis-ci.org/sloria/gig)
+
+A CLI to create .gitignore files, to keep your source control so fresh and clean.
+
+![Andre](https://upload.wikimedia.org/wikipedia/commons/d/d8/Andr%C3%A93000.jpg)
+
+## Install/Upgrade
+
+```
+$ pip install -U gig
+```
+
+Or, run it with [pipx](https://github.com/pipxproject/pipx):
+
+```
+$ pipx run gig --help
+```
+
+## Usage
+
+In the shell. . .
+
+```
+$ gig Python Ruby > .gitignore
+```
+
+Or, programmatically, in Python. . .
+
+```python
+>>> import gig
+>>> gig.generate_gitignore(["Clojure"])
+u'\n### Clojure ###\nLeiningen.gitignore'
+```
+
+You can also generate global `.gitignore_global` files. For more info, see [here](https://help.github.com/en/articles/ignoring-files#create-a-global-gitignore).
+
+```
+$ gig macOS VisualStudioCode --global > ~/.gitignore_global
+```
+
+To get a list of available templates. . .
+
+```
+$ gig list
+$ gig list --global
+```
+
+For more help, run `gig --help`.
+
+## Requirements
+
+- Python >= 3.6
+- Internet connection
+
+## License
+
+MIT Licensed.
+
+## Changelog
+
+### 1.0.0
+
+- Write to `stdout`.
+- Only Python>=3.6 is supported.
+
+### 0.2.0 (09/09/2013)
+
+- Add support for global gitignore files.
+
+### 0.1.0 (09/07/2013)
+
+- First release
--- tests/test_gig.py
+++ tests/test_gig.py
@@ -1,90 +1,40 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-import os
-import unittest
-import logging
-from nose.tools import *  # PEP8 asserts
-from subprocess import check_output, CalledProcessError
-
-import gig
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-class TestGig(unittest.TestCase):
-
-    '''Unit tests for gig.'''
-
-    def setUp(self):
-        os.chdir(HERE)
-
-    def tearDown(self):
-        os.chdir(HERE)
-        if os.path.isfile('.gitignore'):
-            os.remove('.gitignore')
-
-    def test_single_language(self):
-        run_cmd("gig Python")
-        assert_true(os.path.isfile('.gitignore'))
-        with open('.gitignore', 'r') as fp:
-            contents = fp.read()
-            assert_in('*.py[cod]', contents)
-            assert_in('### Python ###', contents)
-
-    def test_multiple_languages(self):
-        run_cmd('gig Python Ruby')
-        assert_true(os.path.isfile('.gitignore'))
-        with open('.gitignore', 'r') as fp:
-            gitignore = fp.read()
-            assert_in("*.py[cod]", gitignore)
-            assert_in("*.gem", gitignore)
-            assert_in("### Python ###", gitignore)
-            assert_in("### Ruby ###", gitignore)
-
-    def test_cant_overwrite_by_default(self):
-        run_cmd("gig Python")
-        # Now a .gitignore exists, shouldn't be able to overwrite
-        with assert_raises(CalledProcessError):
-            run_cmd('gig Ruby')
-
-    def test_force(self):
-        run_cmd('gig Python')
-        run_cmd('gig Ruby -f')
-        with open('.gitignore', 'r') as fp:
-            gitignore = fp.read()
-            assert_in('### Ruby ###', gitignore)
-            assert_in("*.gem", gitignore)
-            assert_not_in("### Python ###", gitignore)
-
-    def test_append(self):
-        run_cmd('gig Python')
-        run_cmd('gig Ruby -a')
-        run_cmd('gig Clojure --append')
-        with open('.gitignore', 'r') as fp:
-            gitignore = fp.read()
-            assert_in("### Ruby ###", gitignore)
-            assert_in("*.py[cod]", gitignore)
-            assert_in("### Python ###", gitignore)
-            assert_in("*.gem", gitignore)
-            assert_in("### Clojure ###", gitignore)
-            assert_in("Leiningen.gitignore", gitignore)
-
-    def test_generate_gitignore(self):
-        gitignore = gig.generate_gitignore(['Python', 'Ruby'])
-        assert_in("*.py[cod]", gitignore)
-        assert_in("*.gem", gitignore)
-        assert_in("### Python ###", gitignore)
-        assert_in("### Ruby ###", gitignore)
-
-    def test_generate_global_gitignore(self):
-        gitignore = gig.generate_gitignore(["Windows", "OSX"], global_=True)
-        assert_in("Thumbs.db", gitignore)
-        assert_in(".DS_Store", gitignore)
+import pytest
+from scripttest import TestFileEnvironment as FileEnvironment
 
 
-def run_cmd(cmd):
-    '''Run a shell command `cmd` and return its output.'''
-    return check_output(cmd, shell=True).decode('utf-8')
+@pytest.fixture
+def env():
+    return FileEnvironment()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_no_arguments(env):
+    res = env.run("gig", expect_error=True)
+    assert res.returncode == 1
+    assert "Usage:" in res.stderr
+
+
+def test_single_language(env):
+    res = env.run("gig", "Python")
+    assert res.returncode == 0
+    assert "### Python ###" in res.stdout
+
+
+def test_multiple_languages(env):
+    res = env.run("gig", "Python", "Ruby")
+    assert "*.py[cod]" in res.stdout
+    assert "*.gem" in res.stdout
+    assert "### Python ###" in res.stdout
+    assert "### Ruby ###" in res.stdout
+
+
+def test_generate_global_gitignore(env):
+    res = env.run("gig", "Windows", "macOS", "--global")
+    assert "Thumbs.db" in res.stdout
+    assert ".DS_Store" in res.stdout
+    assert "### Windows ###" in res.stdout
+    assert "### macOS ###" in res.stdout
+
+
+def test_error(env):
+    res = env.run("gig", "notfound", expect_error=True)
+    assert res.returncode == 1
